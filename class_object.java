3e6021f94a4314758577e335ad49a1581d952773
Ganeshbabu26 <ganeshbabumadhavan@gmail.com>
2025-06-22 13:15:56 +0530

    updated

--- class_object.java
+++ class_object.java
@@ -21,5 +21,9 @@ public class class_object
         r.getDetails(10, 20);
         int area = r.area();
         System.out.print(area);
+        r.length = 30; // Modifying the length
+        r.width = 40;  // Modifying the width
+        area = r.area(); // Recalculating area after modification
+        System.out.print(" " + area); // Output the new area
     }
 }
